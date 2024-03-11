c6fdd1d82ce6dc3ca395b9ee3ed48410e785cbe2
Beyza Usta <129887098+beyzqusta@users.noreply.github.com>
2024-03-11 17:41:42 +0300

    Create try-except.py

---
+++ try-except.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def çift_mi(sayı):
+    
+    if (sayı % 2 == 0):
+        return sayı
+    else:
+        raise ValueError
+liste = [34,2,1,3,33,100,61,1800]
+
+for i in liste:
+    try:
+        print(çift_mi(i))
+    except ValueError:
+        pass
+        
+
+
+# In[ ]:
+
+
+
+
