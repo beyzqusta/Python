c7bfc1fd32a8ba017893bbd694052c3b43ef3a5b
Beyza Usta <129887098+beyzqusta@users.noreply.github.com>
2024-03-11 17:44:48 +0300

    Create bilgisayarı kapatır.py

---
+++ bilgisayarı kapatır.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import os
+shutdown = input("Do you want to shutdown your computer (yes / no): ")
+if shutdown == 'yes':
+    os.system("shutdown /s /t 1")
+else:
+    print('Shutdown is not requested')       
+
