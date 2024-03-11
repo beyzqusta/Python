fe297fb6f6a5126e802fcc11b8555c1f7a120948
Beyza Usta <129887098+beyzqusta@users.noreply.github.com>
2024-03-11 17:41:59 +0300

    Create iterator kullanımı.py

---
+++ iterator kullanımı.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def asal_mı(sayı):
+    i =  2
+    
+    while i < sayı:
+        if (sayı % i == 0):
+            return False
+        i += 1
+    return True
+def asal_generator():
+    i =  2
+    while True:
+        if (asal_mı(i)):
+            yield i
+        i += 1
+
+for sayı in asal_generator():
+    if (sayı > 1000):
+        break
+    print(sayı)
+        
+
+
+# In[ ]:
+
+
+
+
